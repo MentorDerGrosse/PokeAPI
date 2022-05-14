da27632ce7318d4323e4c151abc113f557f6a701
emilwache <96784386+emilwache@users.noreply.github.com>
2022-05-15 00:03:13 +0200

    update pokeapi (Emil und Thomas)

--- Pokedex/js/main.js
+++ Pokedex/js/main.js
@@ -1,14 +1,18 @@
-window.onload = function () {
 
-  //höchster index ist 898
-  let url = 'https://pokeapi.co/api/v2/pokemon/';
+window.onload = function() {
+
+  //https://pokeapi.co/api/v2/pokemon/
+  //höchster index = 898
+  //let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9';
+  let url = 'https://pokeapi.co/api/v2/pokemon/2';
+
   fetch(url)
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      readObject(data);
+      //console.log(data.stats[0])
+      //readObject(data);
     });
-    document.getElementById('abc').innerHTML = pokeInnerHTML;
 };
 
 function readObject(data){
@@ -18,16 +22,47 @@ function readObject(data){
     row += buildTableEntry(character, index); 
     index++;
   });
-  document.querySelector('#img_test').innerHTML = row;
+  document.querySelector('#row-pokemon').innerHTML = row;
 }
 
 
 function buildTableEntry(character, index) {
-  console.log("it works")
-  return (img = `
-  <div class="img-container">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png" alt="${name}" height="200" width="200"/>
-      <p>${character.name}</p>
-  </div>
-`);
+  let url = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+  var hp = 0;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      hp = data.stats[0].base_stat;
+      console.log(hp);
+
+      let cardElement = `
+    <div class='col-md-4'>
+      <br />
+      <div class='card card-elements'>
+        <div class='row card-head'>
+          <div class='col-6 pokemon-name'> 
+            ${character.name}
+          </div>
+          <div class='col-6 pokemon-index'> 
+            ${hp}
+          </div>
+        </div>
+
+        <img
+          class='card-img-top'
+          id='image'
+          src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png"
+          alt='${character.name}'
+        />
+        <div class='card-body'>
+          <h4 id='name'>Name: ${character.name}</h4>
+        </div>
+      </div>
+    </div>
+  `;
+  console.log(cardElement);
+  return cardElement;
+
+    });
+    
 };
