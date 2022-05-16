eeddce4842c0a67414ef3936a7025adc9a696a72
emilwache <96784386+emilwache@users.noreply.github.com>
2022-05-16 12:02:13 +0200

    card design für jetzt fertig (emil + thomas)

--- Pokedex/js/main.js
+++ Pokedex/js/main.js
@@ -1,8 +1,8 @@
 window.onload = function() {
 
   //https://pokeapi.co/api/v2/pokemon/
-  //hÃ¶chster index = 898
-  let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9';
+  //höchster index = 898
+  let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=27';
   //let url = 'https://pokeapi.co/api/v2/pokemon/2';
 
   fetch(url)
@@ -27,10 +27,17 @@ async function readObject(data){
 async function buildTableEntry(character, index) {
 
   let url = `https://pokeapi.co/api/v2/pokemon/${index}/`;
-  var hp = 0;
+  let hp = 0;
+  let type, attack, defense, specialAttack, speacialDefense, speed, weight;
+
   data = await (await fetch(url)).json()
   hp = data.stats[0].base_stat;
-  console.log(hp);
+  attack = data.stats[1].base_stat;
+  defense = data.stats[2].base_stat;
+  specialAttack = data.stats[3].base_stat;
+  speacialDefense = data.stats[4].base_stat;
+  speed = data.stats[5].base_stat;
+  weight = data.weight;
 
     let cardElement = `
     <div class='col-md-4'>
@@ -40,19 +47,43 @@ async function buildTableEntry(character, index) {
           <div class='col-6 pokemon-name'>
             ${character.name}
           </div>
-          <div class='col-6 pokemon-index'>
-            ${hp}
+          <div class='col-6 pokemon-hp'>
+            <span id='hp'>HP</span>${hp}
           </div>
         </div>
 
         <img
           class='card-img-top'
-          id='image'
           src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png"
           alt='${character.name}'
         />
         <div class='card-body'>
-          <h4 id='name'>Name: ${character.name}</h4>
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Attack: </span> <span class="info-num">${attack}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Defense: </span> <span class="info-num">${defense}</span>
+          </div>
+
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Speacial Attack: </span> <span class="info-num">${specialAttack}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Speacial Defense: </span> <span class="info-num">${speacialDefense}</span>
+          </div>
+
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Speed: </span> <span class="info-num">${speed}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Weight: </span> <span class="info-num">${weight}</span>
+          </div>
+        </div>
+        </div>
+        </div>
         </div>
       </div>
     </div>
