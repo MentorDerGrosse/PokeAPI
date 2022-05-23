5ea1c468f287d692f008daa09a2587f96738835b
emilwache <96784386+emilwache@users.noreply.github.com>
2022-05-23 21:45:06 +0200

    next Page funktioniert, das gleiche kann man einfach mit previous page machen

--- Pokedex/js/main.js
+++ Pokedex/js/main.js
@@ -1,13 +1,24 @@
+class Pokemon {
+  constructor (name, hp, attack, defense, specialAttack, speacialDefense, speed, weight, element) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.specialAttack = specialAttack;
+    this.speacialDefense = speacialDefense;
+    this.speed = speed;
+    this.weight = weight;
+    this.element = element;
+  }
+}
 
-  var start = 0;
-  var ende = 15;
+var currentIndex = 15;
 
 window.onload = function starting() {
-  console.log(start);
   localStorage.clear();
   //https://pokeapi.co/api/v2/pokemon/
   //höchster index = 898
-  let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898';
   //let url = 'https://pokeapi.co/api/v2/pokemon/2';
 
   fetch(url)
@@ -22,16 +33,18 @@ async function readObject(data){
   let row = '';
   let index = 1;
   for(const character of data.results) {
-    if(start <= index && index <= ende) {
+    if(index <= 15) {
     row += await buildTableEntry(character, index);
     console.log(element);
-    index++;
     }
+    if(index > 15){
+      savePokemons(character, index);
+    }
+    index++;
   };
   document.querySelector('#row-pokemon').innerHTML = row;
  
 }
-
 
 async function buildTableEntry(character, index) {
 
@@ -49,28 +62,10 @@ async function buildTableEntry(character, index) {
   weight = data.weight;
   element = data.types[0].type.name;
 
-  
-
-  class Pokemon {
-    constructor (name, hp, attack, defense, specialAttack, speacialDefense, speed, weight, element) {
-      this.name = name;
-      this.hp = hp;
-      this.attack = attack;
-      this.defense = defense;
-      this.specialAttack = specialAttack;
-      this.speacialDefense = speacialDefense;
-      this.speed = speed;
-      this.weight = weight;
-      this.element = element;
-    }
-  }
-
   poke = new Pokemon(data.name, hp, attack, defense, specialAttack, speacialDefense, speed, weight, element);
 
   localStorage.setItem(index, JSON.stringify(poke));
   
-
-
     let cardElement = `
     <div class='col-md-4'>
       <br />
@@ -120,15 +115,85 @@ async function buildTableEntry(character, index) {
       </div>
     </div>
   `;
-
-  
-
   return cardElement;
   
 };
 
-function up(data) {
-  start = start + 15;
-  ende = ende + 15;
-  readObject(data);
+function savePokemons(character, index){
+  let url = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+  let hp, type, attack, defense, specialAttack, speacialDefense, speed, weight;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      hp = data.stats[0].base_stat;
+      attack = data.stats[1].base_stat;
+      defense = data.stats[2].base_stat;
+      specialAttack = data.stats[3].base_stat;
+      speacialDefense = data.stats[4].base_stat;
+        speed = data.stats[5].base_stat;
+      weight = data.weight;
+      element = data.types[0].type.name;
+      poke = new Pokemon(data.name, hp, attack, defense, specialAttack, speacialDefense, speed, weight, element);
+      localStorage.setItem(index, JSON.stringify(poke));
+    })
 }
+
+function nextPage(){
+  nextIndex = currentIndex + 15;
+  document.querySelector('#row-pokemon').innerHTML = "";
+  for(i = currentIndex+1; i<=nextIndex; i++){
+    poke = JSON.parse(localStorage.getItem(i));
+    console.log(i, nextIndex);
+    let cardElement = `
+    <div class='col-md-4'>
+      <br />
+      <div class='card card-elements' id='background'>
+        <div class='row card-head'>
+          <div class='col-6 pokemon-name'>
+            ${poke.name}
+          </div>
+          <div class='col-6 pokemon-hp'>
+            <span id='hp'>HP</span>${poke.hp}
+          </div>
+        </div>
+
+        <img
+          class='card-img-top'
+          src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png"
+          alt='${poke.name}'
+        />
+        <div class='card-body'>
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Attack: </span> <span class="info-num">${poke.attack}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Defense: </span> <span class="info-num">${poke.defense}</span>
+          </div>
+
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Speacial Attack: </span> <span class="info-num">${poke.specialAttack}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Speacial Defense: </span> <span class="info-num">${poke.speacialDefense}</span>
+          </div>
+
+          <div class='row card-bottom'>
+            <div class='col-6 column-info text-end'>
+              <span class="card-info">Speed: </span> <span class="info-num">${poke.speed}</span>
+            </div>
+          <div class='col-6 column-info text-end'>
+            <span class="card-info">Weight: </span> <span class="info-num">${poke.weight}</span>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  `;
+  document.querySelector('#row-pokemon').innerHTML += cardElement;
+  }
+  currentIndex += 15;
+};
